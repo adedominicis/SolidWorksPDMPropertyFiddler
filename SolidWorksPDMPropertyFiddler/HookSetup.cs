4e4d3cf0cd3ed3552577b195a14c77f78d566d63
PowertrainFSAE47 <dominicis.a@gmail.com>
2021-07-19 19:36:31 -0400

    Agregada ventana WPF para integrar funcionalidad.

--- SolidWorksPDMPropertyFiddler/HookSetup.cs
+++ SolidWorksPDMPropertyFiddler/HookSetup.cs
@@ -1,11 +1,13 @@
 ﻿using EPDM.Interop.epdm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace SolidWorksPDMPropertyFiddler
 {
@@ -15,16 +17,19 @@ namespace SolidWorksPDMPropertyFiddler
         private EdmVault5 thisVault;
         private IEdmVault8 thisVaultV8;
         private IWin32Window parentWnd;
+
+
+        ObservableCollection<string> fileList;
         public void GetAddInInfo(ref EdmAddInInfo poInfo, IEdmVault5 poVault, IEdmCmdMgr5 poCmdMgr)
         {
-            //MessageBox.Show(String.Format(" Attach debugger to process {0} (ID: {1})", System.Diagnostics.Process.GetCurrentProcess().ProcessName, System.Diagnostics.Process.GetCurrentProcess().Id));
+
             try
             {
                 //Información miscelanea del add-in
                 poInfo.mbsAddInName = "PropertyFiddler";
                 poInfo.mbsCompany = "ADDR";
                 poInfo.mbsDescription = string.Format("Batch property setup");
-                poInfo.mlAddInVersion = 1;
+                poInfo.mlAddInVersion = 2;
                 poInfo.mlRequiredVersionMajor = 5;
                 poInfo.mlRequiredVersionMinor = 2;
 
@@ -44,8 +49,52 @@ namespace SolidWorksPDMPropertyFiddler
 
         public void OnCmd(ref EdmCmd poCmd, ref EdmCmdData[] ppoData)
         {
-            MessageBox.Show("You clicked!");
+            try
+            {
+                if (poCmd.meCmdType == EdmCmdType.EdmCmd_Menu)
+                {
+                    fileList= new ObservableCollection<string>();
+
+                    //enableDebugger();
+                    MessageBox.Show("Elegiste el menu");
+                    string fileName;
+                    for (int i = 0; i < ppoData.Length; i++)
+                    {
+                        fileName = ppoData[i].mbsStrData1;
+                        fileList.Add(Path.GetFileName(fileName));
+                    }
+                }
+                UI miVentana = new UI();
+                
+                
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+           
+
         }
+
+        #region Métodos privados
+        private void enableDebugger()
+        {
+            MessageBox.Show(String.Format(" Attach debugger to process {0} (ID: {1})", System.Diagnostics.Process.GetCurrentProcess().ProcessName, System.Diagnostics.Process.GetCurrentProcess().Id));
+        }
+        private bool isAdminLogged()
+        {
+            IEdmUserMgr5 userMgr = default(IEdmUserMgr5);
+            IEdmUser5 usuario = default(IEdmUser5);
+
+            userMgr = (IEdmUserMgr5)thisVault;
+            usuario = userMgr.GetLoggedInUser();
+
+            return string.Equals(usuario.Name, "admin", StringComparison.OrdinalIgnoreCase);
+
+        }
+
+        #endregion
     }
 }
 
---
+++ SolidWorksPDMPropertyFiddler/UI.xaml.cs
@@ -0,0 +1,34 @@
+﻿using SolidWorksPDMPropertyFiddler.viewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SolidWorksPDMPropertyFiddler
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UI : Window
+    {
+        private ViewModel vm;
+        public UI()
+        {
+            vm = new ViewModel();
+            this.DataContext = vm;
+            InitializeComponent();
+            this.ShowDialog();
+
+        }
+    }
+}
